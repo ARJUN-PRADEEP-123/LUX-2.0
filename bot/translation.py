d0280f5ab317c25ff81c067c9d9978d06641c25b
ARJUN-PRADEEP-123 <93504468+ARJUN-PRADEEP-123@users.noreply.github.com>
2021-11-06 19:31:24 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,7 +4,7 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}How are youu!!</b>
+    START_TEXT = """<b><u>Hey {}How are youu!!</b><u?
 <i>Hii I am LUX 2.0.This is my mom -@Its_Me_LuX_bot (LUX)
 
 U can't add Me To Your Group And Channel,
@@ -53,11 +53,11 @@ For more details about me.Contact..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Arjunbots">Arjun Pradeep</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/ARJUN-PRADEEP-123/LUX-2.0">Click Me</a></i>
 """
